47cfe61a829b66f93c67200e216b044df1236fb5
grioool <olga.a.grigorieva@gmail.com>
2025-02-26 17:56:13 +0100

    update admin endpoint with analytic

--- api/admin/admin_router.py
+++ api/admin/admin_router.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, HTTPException
 
-from api.admin.admin_service import get_analytics_data
+from api.admin.admin_service import get_analytics_data, count_users
 from db.config import SessionDep
 
 router = APIRouter()
@@ -9,10 +9,11 @@ router = APIRouter()
 @router.get("/admin/analytics")
 async def get_admin_analytics(session: SessionDep):
     analytics_data = await get_analytics_data(session)
+    total_users = await count_users(session)
     if analytics_data:
         return {
             "total_summaries": analytics_data.total_summaries,
-            "total_users": analytics_data.total_users
+            "total_users": total_users
         }
     else:
         raise HTTPException(status_code=404, detail="Analytics data not found")
--- api/admin/admin_service.py
+++ api/admin/admin_service.py
@@ -1,6 +1,8 @@
+from sqlalchemy import func
 from sqlmodel import select
 
 from api.admin.analytic import Analytic
+from api.user.user import User
 from db.config import SessionDep
 
 
@@ -8,3 +10,9 @@ async def get_analytics_data(session: SessionDep) -> Analytic:
     statement = select(Analytic).order_by(Analytic.id.desc()).limit(1)
     result = session.exec(statement)
     return result.first()
+
+
+async def count_users(session: SessionDep) -> int:
+    result = session.execute(select(func.count(User.id)))
+    total_users = result.scalar()
+    return total_users
