3f7d3033d815e210575879d45447efee71af81fe
grioool <olga.a.grigorieva@gmail.com>
2025-03-01 22:45:04 +0100

    add implementation for summary download

--- api/summary/download_router.py
+++ api/summary/download_router.py
@@ -1,15 +1,25 @@
 from datetime import datetime
 
 from fastapi import APIRouter, HTTPException
-from google.cloud.storage import bucket
+from google.cloud import storage
+from fastapi.responses import FileResponse
+
+storage_client = storage.Client()
+bucket_name = "cbts-bucket"
+bucket = storage_client.bucket(bucket_name)
 
 router = APIRouter()
 
 @router.get("/download/{filename}")
 async def download_file(filename: str):
-    blob = bucket.blob(f"uploads/{filename}")
+    blob = bucket.blob(f"summaries/{filename}.txt")
     if not blob.exists():
-        raise HTTPException(status_code=404, detail="File not found")
+        raise HTTPException(status_code=404, detail="Summary not found")
+    tmp_path = f"/tmp/{filename}"
+    blob.download_to_filename(tmp_path)
 
-    url = blob.generate_signed_url(version="v4", expiration=datetime.timedelta(minutes=15), method="GET")
-    return {"url": url}
+    return FileResponse(
+        path=tmp_path,
+        media_type="text/plain",
+        filename=f"{filename}.txt"
+    )
