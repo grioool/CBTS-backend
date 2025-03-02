92e41bac685db9d335fdcb6e3e9755d8041325d2
grioool <olga.a.grigorieva@gmail.com>
2025-03-02 19:42:50 +0100

    add user history

--- api/summary/summary.py
+++ api/summary/summary.py
@@ -4,4 +4,5 @@ from sqlmodel import Field, SQLModel
 class Summary(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
     filename: str
+    filename_hash: str
     user_id: int = Field(foreign_key="user.id")
