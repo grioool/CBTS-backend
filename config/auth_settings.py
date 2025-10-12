904c438a9975e16ee50420570ac85bc02369be38
grioool <olga.a.grigorieva@gmail.com>
2025-10-12 19:09:27 +0200

    add expire time

--- config/auth_settings.py
+++ config/auth_settings.py
@@ -4,7 +4,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class AuthSettings(BaseSettings):
     SECRET_KEY: str
     ALGORITHM: str
-    ACCESS_TOKEN_EXPIRE_MINUTES: int
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
     FORGOT_PASSWORD_URL: str
 
     model_config = SettingsConfigDict(env_file=".env", extra="allow")
