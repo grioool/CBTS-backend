cb858729bc9637d470356a1bf961c1804eaedd96
grioool <olga.a.grigorieva@gmail.com>
2025-10-12 20:31:49 +0200

    fix admin tab

--- api/admin/admin_service.py
+++ api/admin/admin_service.py
@@ -6,13 +6,13 @@ from api.user.entity.user import User
 from db.config import SessionDep
 
 
-def count_users(session: SessionDep) -> int:
+async def count_users(session: SessionDep) -> int:
     result = session.execute(select(func.count(User.id)))
     total_users = result.scalar()
     return total_users
 
 
-def count_summaries(session: SessionDep) -> int:
+async def count_summaries(session: SessionDep) -> int:
     result = session.execute(select(func.count(Summary.id)))
     total_summaries = result.scalar()
     return total_summaries
